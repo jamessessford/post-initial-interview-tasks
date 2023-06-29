15107bb1fa11cd106cb61e28e6067845fea6e56b
Nathan Zagrovic <nathan@innercitydigital.co.uk>
2023-06-29 17:45:01 +0100

    App setup for test

---
+++ app/Models/Task.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+use App\Models\Category;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'status',
+        'description',
+        'due_date',
+        'hours_required',
+        'category_id'
+    ];
+
+    public function category() {
+        return $this->belongsTo(Category::class);
+    }
+
+}
