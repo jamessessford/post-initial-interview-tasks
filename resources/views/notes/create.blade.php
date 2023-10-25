1bbb9618848d81812823dfce6341abb189dc80f9
Louis Robbins <louis.robbins96@outlook.com>
2023-10-25 15:00:51 +0100

    Update create.blade.php
    adding text colours to create note blade file

--- resources/views/notes/create.blade.php
+++ resources/views/notes/create.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="container mx-auto p-4">
-        <h2 class="text-3xl font-semibold mb-4">Add a Note</h2>
+        <h2 class="text-3xl font-semibold mb-4 text-white">Add a Note</h2>
 
         <form method="post" action="{{ route('complaints.notes.store', ['complaint' => $complaint->id]) }}">
             @csrf
@@ -12,7 +12,7 @@
                 <textarea name="content" id="content" class="form-textarea w-full" rows="4" required></textarea>
             </div>
 
-            <button type="submit" class="btn btn-primary">Add Note</button>
+            <button type="submit" class="btn btn-primary text-indigo-600 hover:underline">Add Note</button>
         </form>
     </div>
 @endsection
